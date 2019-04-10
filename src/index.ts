b5dca85a6dadfd806d4490b1f17c099c5df54f20
Charles Cunningham <charles.cunningham@outlook.com>
2019-04-10 13:15:52 +0200

    added custom seed/password parameter passing

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,39 @@
 import * as program from 'commander';
 import Controller from './controller';
 
-const init = async (seed: any, password: string) => {
-  return await Controller(seed, password);
-}
+const init = async (paramlist?: string[]) => {
+  if (paramlist && paramlist.length == 2) {
+    return await Controller(Buffer.alloc(32, paramlist[0], 'hex'), paramlist[1]);
+  }
 
-const seed = Buffer.alloc(32, 'a'.repeat(64));
-const pass = 'secret';
+  return await Controller(Buffer.alloc(32, 'a'.repeat(64), 'hex'), 'secret');
+};
 
 program.version('0.1.0')
-  .option('-s, --seed <seed>', 'choose a seed')
-  .option('-p, --password <password>', 'choose a password');
+  .option('-p, --params <seed>,<password>',
+          'choose a seed (64 digit hex) and password in list form: seed,password',
+          (val: string): string[] => val.split(','));
 
 program.command('did')
   .description('Get basic info (did) for this identity')
   .action(async _ => {
-    const id = await init(seed, pass);
+    const id = program.params ? await init(program.params) : await init();
     console.log(id.getDid());
   });
 
-program.command('generate [type] [args]')
-  .description('Generate a request JWT of any type (defaults to authentication)')
+program.command('generate <type> <args>')
+  .description('Generate a request JWT of any type with args in json form')
   .action(async (type, args) => {
-    const id = await init(seed, pass);
+    const id = await init();
     switch (type) {
       case 'payment':
-        console.log(await id.getPaymentRequest(args));
+        console.log(await id.getPaymentRequest(JSON.parse(args)));
+        break;
+      case 'authentication':
+        console.log(await id.getAuthenticationRequest(JSON.parse(args)));
         break;
       default:
-        console.log(await id.getAuthenticationRequest(args));
-        break;
+        console.log('request type not supported');
     }
   });
 
@@ -37,7 +41,7 @@ program
   .command('validate <response>')
   .description('Validate a JWT given in response to an interaction request')
   .action(async (response) => {
-    const id = await init(seed, pass);
+    const id = await init();
     console.log(await id.isInteractionResponseValid(response))
   });
 
